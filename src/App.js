1685d35e2920f71f58045a0ba742e441b0fdd2c0
monicaep <mep823@gmail.com>
2020-03-25 23:49:07 -0700

    react router & basic routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import './App.css';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Palette list goes here</h1>} />
+        <Route 
+          exact 
+          path='/palette/:id' 
+          render={(routeProps) => <Palette palette={generatePalette(
+            this.findPalette(routeProps.match.params.id))} />
+          }
+        />
+      </Switch>
     );
   }
 }
 
 export default App;
+
+/*
+  <div className="App">
+    <Palette palette={generatePalette(seedColors[4])} />
+  </div>
+*/
