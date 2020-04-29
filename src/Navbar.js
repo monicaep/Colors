9bc7b9b41499243398c9fdbb71e2f9b83f149c8c
monicaep <mep823@gmail.com>
2020-04-28 18:38:20 -0700

    Refactor styles out of Navbar.css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link }  from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Slider from 'rc-slider';
@@ -7,7 +8,44 @@ import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
-import './Navbar.css';
+
+const styles = {
+    navbar: {
+        display: 'flex',
+        alignContent: 'flex-start',
+        alignItems: 'center',
+        height: '6vh'      
+    },
+    logo: {
+        marginRight: '15px',
+        padding: '0 13px',
+        fontSize: '22px',
+        backgroundColor: '#eceff1',
+        fontFamily: 'Roboto',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        '& a': {
+            textDecoration: 'none',
+            color: 'black'
+        }   
+    }, 
+    slider: {
+        width: '340px',
+        margin: '0 10px',
+        display: 'inline-block',
+        '& .rc-slider-track': {
+            backgroundColor: 'transparent'
+        },
+        '& .rc-slider-rail': {
+            height: '8px'
+        }
+    },
+    sliderContainer: {
+        marginLeft: 'auto',
+        marginRight: '1rem'
+    }
+}
 
 class Navbar extends Component {
     constructor(props) {
@@ -27,15 +65,17 @@ class Navbar extends Component {
     }
 
     render() {
+        const { classes } = this.props;
+
         return (
-            <nav className='Navbar'>
-                <div className='logo'>
+            <nav className={classes.navbar}>
+                <div className={classes.logo}>
                     <Link to='/'>reactcolorpicker</Link>
                 </div>
                 {this.props.showingAllColors && (
-                    <div className='slider-container'>
+                    <div className={classes.sliderContainer}>
                         <span>Level: {this.props.level}</span>
-                        <div className='slider'>
+                        <div className={classes.slider}>
                             <Slider 
                                 defaultValue={this.props.level} 
                                 min={100} 
@@ -79,4 +119,4 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
