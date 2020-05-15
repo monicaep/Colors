201852021c01931081f89971c1a4f3c74cbfa8bc
monicaep <mep823@gmail.com>
2020-05-15 13:40:02 -0700

    Updates

---
+++ precache-manifest.d1c20c68e5ace02113782b1ab89161fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0885ac1b8f63d93a893992e2d5997896",
+    "url": "/Colors/index.html"
+  },
+  {
+    "revision": "79f69115f3d8e2c3e341",
+    "url": "/Colors/static/css/2.edd73686.chunk.css"
+  },
+  {
+    "revision": "d66e2f8e73ec3df76fba",
+    "url": "/Colors/static/css/main.232d4e9e.chunk.css"
+  },
+  {
+    "revision": "79f69115f3d8e2c3e341",
+    "url": "/Colors/static/js/2.73c6864f.chunk.js"
+  },
+  {
+    "revision": "6194c42717583939b4f76332be3503d4",
+    "url": "/Colors/static/js/2.73c6864f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66e2f8e73ec3df76fba",
+    "url": "/Colors/static/js/main.1610c618.chunk.js"
+  },
+  {
+    "revision": "9dfbbbd35312fb08651b",
+    "url": "/Colors/static/js/runtime-main.643fcfeb.js"
+  },
+  {
+    "revision": "b582a67d25862ebdbc8e5c297561ec34",
+    "url": "/Colors/static/media/background.b582a67d.svg"
+  }
+]);
